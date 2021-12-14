827f34ad8f0ebc042becbf47d8df03358c993d0b
Rishabh11200 <rishabhshah1100@gmail.com>
2021-12-14 12:22:12 +0530

    iOS configuration

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React, {useEffect} from 'react';
+import {SafeAreaView, StyleSheet, Text, View} from 'react-native';
+import SplashScreen from 'react-native-splash-screen';
+
+const App = () => {
+  useEffect(() => {
+    SplashScreen.hide();
+  }, []);
+  return (
+    <SafeAreaView>
+      <View>
+        <Text>Hello</Text>
+      </View>
+    </SafeAreaView>
+  );
+};
+
+export default App;
